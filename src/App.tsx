205393545f2dd7b62ff51f8084f592f9a883acdd
unknown <griselda.neysa@ui.ac.id>
2024-06-08 20:48:28 +0700

    configure github deployment

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import UIUX from './pages/UIUX';
 
 function App () {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
         Switch
