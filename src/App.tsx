4c882ca4f7eadccf60ec5e47da6bc68e0a7d8b68
unknown <griselda.neysa@ui.ac.id>
2024-06-08 20:51:19 +0700

    configure github deployment

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import UIUX from './pages/UIUX';
 
 function App () {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='https://griseldans.github.io/portfolio'>
       <Routes>
         <Route path="/" element={<Layout />}>
         Switch
